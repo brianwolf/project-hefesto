5ef294ca7ed1b1fb1be305f4b75b317adf061382
Brian <brian.d.lobo@gmail.com>
2022-03-18 10:32:17 -0300

    Se agrega el branch al git clone

--- repo_modules/git_clone.py
+++ repo_modules/git_clone.py
@@ -8,9 +8,16 @@ def exec(workingdir: str, config: Dict[str, str]):
     url = config['url'].replace('https://', '')
     username = config.get('user', None)
     password = config.get('pass', None)
+    branch = config.get('branch', None)
 
-    if not username:
-        sh(f'git clone https://{url}')
-        return
+    cmd = "git clone https://"
 
-    sh(f'git clone https://{username}:{password}@{url}', echo=False)
+    if username and password:
+        cmd += f"{username}:{password}@"
+
+    cmd += url
+
+    if branch:
+        cmd += f' -b {branch}'
+
+    sh(cmd, echo=False)
