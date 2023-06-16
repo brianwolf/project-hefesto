62675df430dbed441a4f427c8dc15d784a090919
Brian Lobo <brian.d.lobo@gmail.com>
2023-06-15 21:33:01 -0300

    Se agrega variable de ambiente

--- logic/apps/pipeline/service.py
+++ logic/apps/pipeline/service.py
@@ -3,7 +3,7 @@ from importlib.util import module_from_spec, spec_from_file_location
 import sys
 
 import yaml
-from jinja2 import Template
+from jinja2 import Template, Environment
 
 from logic.apps.filesystem import workingdir_service
 from logic.apps.modules import service as module_service
@@ -11,11 +11,20 @@ from logic.apps.pipeline.error import PipelineErrors
 from logic.libs.exception.exception import AppException
 
 
-def exec(pipeline_str: str, params: dict[str, any] = {}) -> str:
+def _jinja_template() -> Environment:
+    def env(value, key):
+        return os.getenv(key, value)
 
+    env_template = Environment()
+    env_template.filters["env"] = env
+
+    return env_template
+
+
+def exec(pipeline_str: str, params: dict[str, any] = {}) -> str:
     id = workingdir_service.create()
 
-    pipeline_str = Template(pipeline_str).render(params)
+    pipeline_str = _jinja_template().from_string(pipeline_str).render(params)
     pipeline = _get_pipeline_dict(pipeline_str)
 
     try:
@@ -25,12 +34,11 @@ def exec(pipeline_str: str, params: dict[str, any] = {}) -> str:
         os.chdir(workindir)
 
         for stage in pipeline:
-
-            module_name = stage['module']
+            module_name = stage["module"]
             module_path = _get_module_path(module_name)
 
             if not os.path.exists(module_path):
-                msj = f'Module with name as {module_name} not exist'
+                msj = f"Module with name as {module_name} not exist"
                 raise AppException(PipelineErrors.MODULE_NOT_EXISTS_ERROR, msj)
 
             spec = spec_from_file_location(module_name, module_path)
@@ -54,11 +62,13 @@ def _get_pipeline_dict(pipeline_str: str) -> dict[str, any]:
 
 
 def _get_module_path(module_name: str) -> str:
-
-    module_path = f'{module_service.get_path()}/{module_name}.py'
+    module_path = f"{module_service.get_path()}/{module_name}.py"
 
     # para que funcione al estar compilado
-    module_path = f'{sys._MEIPASS}/{module_path}' if hasattr(
-        sys, '_MEIPASS') else f'../{module_path}'
+    module_path = (
+        f"{sys._MEIPASS}/{module_path}"
+        if hasattr(sys, "_MEIPASS")
+        else f"../{module_path}"
+    )
 
     return module_path
